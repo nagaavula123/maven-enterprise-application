74056477101dec2cc5287a500f6305311d9c260d
nagaavula <nagaavula123@gmail.com>
2024-02-21 15:22:18 +0530

    naga

---
+++ createrepo.sh
@@ -0,0 +1,11 @@
+echo "Creating the repo...."
+curl \
+  -X post \
+  -u nagaavula123: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 \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://github.com/orgs/mss-ec-apps-apr/repos \
+  -d '{"name":"Wallmart-GitHubAPIRepo"}'
+~
+~
+~
+	  
